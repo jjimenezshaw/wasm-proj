542bcda7ba75eb2e64b35800fb03aaa367850755
Javier Jimenez Shaw <j1@jimenezshaw.com>
2026-02-23 15:26:08 +0100

    add run url param ot transform.js

--- web/transform.js
+++ web/transform.js
@@ -109,6 +109,7 @@ function loadFromURLParams(crs_list) {
     for (let id of ['source-horizontal-input', 'source-vertical-input', 'target-horizontal-input', 'target-vertical-input']) {
         document.getElementById(id).title = document.getElementById(id).value;
     }
+    return params.get('run') === '1';
 }
 
 function validateForm() {
@@ -309,8 +310,8 @@ function clearField(targetId) {
     // Dispatch an input event so your validation, metadata, and URL updating functions run automatically
     el.dispatchEvent(new Event('input', { bubbles: true }));
     // The ideas was to keep the user's cursor in the box
-    // but Chrome is taking a long time... and it is not worth it. Just press <tab>
-    // el.focus();
+    // but Chrome is taking a long time... let's do it for now.
+    el.focus();
 }
 
 // prefix: source, target
@@ -570,7 +571,7 @@ async function load() {
     setupCustomCombobox('target', 'horizontal', horizontalData, crs_list);
     setupCustomCombobox('target', 'vertical', verticalData, crs_list);
 
-    loadFromURLParams(crs_list);
+    const run = loadFromURLParams(crs_list);
     manageVertical('source', crs_list);
     manageVertical('target', crs_list);
 
@@ -588,6 +589,8 @@ async function load() {
     loader.style.display = 'none';
     appContent.classList.remove('loading-state');
     console.log("Ready.", Date());
+
+    if (run) handleTransform(proj_worker);
 };
 
 window.addEventListener('load', load);
